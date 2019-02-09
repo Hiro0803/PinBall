52b2e80dc2e70b3aaf3a495cda89ee4c5af73d6f
山本裕公 <appleandapril2212@gmail.com>
2019-02-09 19:14:24 +0900

    Pinballプロジェクトの課題提出
    Pinballプロジェクトの課題提出になります。よろしくお願いします。

---
+++ Assets/BallController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //スコアの表示
+    //private float Score = 0;
+
+
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+
+
+
+}
